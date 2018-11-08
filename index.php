d8ba2bf8ee3387d8fa5ed1e9a0ec10425d11f952
Samuel Costa <samu.rcosta@gmail.com>
2018-11-08 21:04:58 -0200

    Função de conversão de romano para decimal

--- index.php
+++ index.php
@@ -3,8 +3,4 @@ require 'src/Problema.php';
 
 $p = new Problema();
 
-if($p->verificarBissexto(88)){
-    echo "é bissexto";
-}else{
-    echo "não é";
-}
+echo $p->romanoParaDecimal("CCCLIV");
--- src/Problema.php
+++ src/Problema.php
@@ -16,7 +16,56 @@ class Problema{
         }
     }
 
-    public function romanoParaDecimal(String $romano){
-        return 0;
+    public function romanoParaDecimal(String $numeroRomano){
+        $numeroRomano = strtoupper($numeroRomano);
+
+        $conjunto_convertido = array();
+
+
+        for ($i = 0; $i < strlen($numeroRomano); $i++) {
+            switch ($numeroRomano[$i]) {
+                case 'I':
+                    $conjunto_convertido[] = 1;
+                    break;
+                case 'V':
+                    $conjunto_convertido[] = 5;
+                    break;
+                case 'X':
+                    $conjunto_convertido[] = 10;
+                    break;
+                case 'L':
+                    $conjunto_convertido[] = 50;
+                    break;
+                case 'C':
+                    $conjunto_convertido[] = 100;
+                    break;
+                case 'D':
+                    $conjunto_convertido[] = 500;
+                    break;
+                case 'M':
+                    $conjunto_convertido[] = 1000;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+
+        $soma = 0;
+
+
+        for($i = 0; $i < count($conjunto_convertido); $i++){
+            if(isset($conjunto_convertido[$i + 1])){
+                if($conjunto_convertido[$i] < $conjunto_convertido[$i + 1]){
+                    $soma -= $conjunto_convertido[$i];
+                }else{
+                    $soma += $conjunto_convertido[$i];
+                }
+            }else{
+                $soma += $conjunto_convertido[$i];
+            }
+        }
+
+
+        return $soma;
     }
 }
